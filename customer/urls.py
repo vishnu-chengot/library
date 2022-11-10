2170557cf1eebc1860ffb2ffd7bc7567b2224c60
vishnu-chengot <vishnu.chengot@gmail.com>
2022-11-10 13:37:05 +0530

    final commit

--- customer/urls.py
+++ customer/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from. import views
+app_name='customer'
 
 urlpatterns=[
-  path('chome',views.home),
-  path('clogin',views.login),
-  path('cbookshelf',views.bookshelf),
-  path('caudiobook',views.audiobook),
-  path('ccontact',views.contact),
+  path('chome',views.home,name='home'),
+  path('clogin',views.login,name='login'),
+  path('cbookshelf',views.bookshelf,name='bookshelf'),
+  path('caudiobook',views.audiobook,name='audiobook'),
+  path('ccontact',views.contact,name='contact'),
 ]
